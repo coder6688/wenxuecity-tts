47e0069195fe91ac90eb0e7ab1a425787edceaa2
coder6688 <110425668+coder6688@users.noreply.github.com>
2025-02-02 14:44:21 -0500

    update windows binary name

--- build_win.py
+++ build_win.py
@@ -1,14 +1,18 @@
 import PyInstaller.__main__
+import datetime
 
 # remove build and dist folders
 import os
 os.system('rm -rf build dist')  
+
+# Get current date in YYYYMMDD format
+current_date = datetime.datetime.now().strftime('%Y%m%d')
 
 PyInstaller.__main__.run([
     'src/wxc_gui.py',  # Replace with your main Python file
     '--onefile',            # Package into a single executable
     '--windowed',           # For GUI apps (remove if console app)
     '--icon=assets/icon.ico',       # Optional: add an icon
-    '--name=WenxueCityTTS',
+    f'--name=WenxuecityTTS_{current_date}',  # Add date to executable name
     '--add-data=models/lid.176.bin;models'  # Add the model file to the bundle
 ])
